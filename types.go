88163bf21367caff6cafe198ea4388bcb3a358b5
Dominik <44637946+dominikzorgnotti@users.noreply.github.com>
2026-06-24 10:42:48 +0200

    Add Firmware field to device structures

--- types.go
+++ types.go
@@ -9,6 +9,7 @@ type RawPCIDevice struct {
 	VID        int16  `json:"vid"`
 	DID        int16  `json:"did"`
 	SSID       int16  `json:"ssid"`
+	Firmware   string `json:"firmware"`
 }
 
 // RawDiskDevice holds the raw vendor and model for a storage disk.
@@ -17,6 +18,7 @@ type RawDiskDevice struct {
 	DeviceType string `json:"device_type"`
 	Vendor     string `json:"vendor"`
 	Model      string `json:"model"`
+	Firmware   string `json:"firmware"`
 }
 
 // RawHostData holds the unanalyzed hardware inventory for a single ESXi host.
@@ -39,6 +41,7 @@ type HCLResult struct {
 	Device     string `json:"device"`
 	DeviceType string `json:"device_type"`
 	Instances  int    `json:"number_of_instances"`
+	Firmware   string `json:"current_firmware"`
 	Certified  string `json:"certified"`
 	HCLLink    string `json:"hcl"`
 
